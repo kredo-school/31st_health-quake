e86022f7e9fcf7c41eb11564af59e19b9dd1b38d
MakotoNin <makoto.08262199@gmail.com>
2025-04-14 21:33:25 +0900

    4/14

--- routes/web.php
+++ routes/web.php
@@ -9,13 +9,13 @@ use App\Http\Controllers\HabitController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\RankingController;
+use App\Http\Controllers\RewardsController;
+use App\Http\Controllers\HomeController;
 
 // 習慣設定画面（追加フォーム）
 Route::get('/add_habit', function () {
     return view('add_habit'); // resources/views/add_habit.blade.php
 })->name('add_habit');
-
-
 
 // 習慣の保存
 Route::post('/save_habit', [HabitController::class, 'store'])->name('save_habit');
@@ -23,21 +23,11 @@ Route::post('/save_habit', [HabitController::class, 'store'])->name('save_habit'
 // 習慣の一覧表示
 Route::get('/set-routine', [HabitController::class, 'index'])->name('set-routine');
 
-// トップページ（ログイン状態による振り分け）
-Route::get('/', function () {
-    if (Auth::check()) {
-        return redirect()->route('dashboard');
-    }
-    return view('welcome');
-});
-Route::get('/habits/{id}', [HabitController::class, 'destroy'])->name('delete-habit');
-
-
 // 認証ルート
 Auth::routes();
 
 // ダッシュボード
-Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('dashboard');
+Route::get('/d', [HomeController::class, 'index'])->name('dashboard');
 
 Route::get('/api/tasks/{year}/{month}', [TaskController::class, 'getTasks']);
 
@@ -62,9 +52,37 @@ Route::get('/user-level', [UserLevelController::class, 'show'])->name('user-leve
 Route::get('/calendar', [CalendarController::class, 'index'])->name('calendar.index')->middleware('auth');
 Route::get('/calendar/{date}', [CalendarController::class, 'show'])->name('calendar.show')->middleware('auth');
 
-
-
 Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register');
 Route::post('register', [RegisterController::class, 'register']);
 
 Route::get('/ranking', [RankingController::class, 'index']);
+
+// 報酬関連ルート
+Route::get('/set-rewards', [RewardsController::class, 'index'])->name('rewards.index');
+Route::post('/set-rewards', [RewardsController::class, 'store'])->name('rewards.store');
+Route::get('/set-rewards/{id}/edit', [RewardsController::class, 'edit'])->name('rewards.edit');
+Route::put('/set-rewards/{id}', [RewardsController::class, 'update'])->name('rewards.update');
+Route::delete('/set-rewards/{id}', [RewardsController::class, 'destroy'])->name('rewards.destroy');
+
+Route::get('/bonus', 'App\Http\Controllers\BonusController@index')->name('bonus');
+Route::get('/bonus/paper', 'App\Http\Controllers\BonusController@showPaper')->name('bonus.paper');
+
+Route::get('/habits/{id}', [HabitController::class, 'destroy'])->name('delete-habit');
+
+// Welcome route - this should be the ONLY route for '/'
+Route::get('/', function () {
+    if (Auth::check()) {
+        return redirect()->route('dashboard');
+    }
+    return view('welcome_login');
+})->name('home');
+
+// If you want to access it via /welcome as well
+Route::get('/welcome', function () {
+    return view('welcome_login');
+});
+
+Route::get('/welcome_login', function () {
+    return view('welcome_login');
+});
+
